0fae05b415f03a3fd39e261ddb2229d17383d416
vprudnikov <vovkasmail@gmail.com>
2016-06-03 10:42:28 +0300

    alchemy client

---
+++ alchemy-client/src/main/java/com/github/xbeowulf/microservices/alchemy/AlchemyClientMain.java
@@ -0,0 +1,14 @@
+package com.github.xbeowulf.microservices.alchemy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AlchemyClientMain {
+
+    private static Logger log = LoggerFactory.getLogger(AlchemyClientMain.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
